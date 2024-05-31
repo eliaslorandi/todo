e5fa00be7574682ecd80ef96d583536d5728b4c0
Elias Lorandi <lorandi.elias@gmail.com>
2024-05-31 12:19:12 -0300

    Seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Category::create([
+            'title' => 'Category Example',
+            'color' => '#FFFFFF',
+            'user_id' => 1
+        ]);
+
+    }
+}
---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Task::create([
+            'title' => 'Task Example',
+            'description' => 'blablablabla',
+            'due_date' => '2024-12-12 00:00:00',
+            'user_id' => 1,
+            'category_id' => 1
+        ]);
+    }
+}
