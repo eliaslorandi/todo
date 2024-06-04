67954141817755c44e17d700b3a092dad733c91a
Elias Lorandi <lorandi.elias@gmail.com>
2024-06-03 22:42:48 -0300

    Continuação layout

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -14,7 +14,43 @@
 </head>
 
 <body>
-    a
+    <div class="container">
+        <div class="sidebar">
+            Logo
+        </div>
+        <div class="content">
+            <nav>
+                <a href="#" class="btn btn-primary">
+                    Nova Tarefa
+                </a>
+            </nav>
+            <main>
+                <section class="graph">
+                    <div class="graph_header">
+                        <h2> Progresso </h2>
+                        <hr class="line_header" />
+                        data
+                    </div>
+                    <div class="graph_header-subtitle">
+                        Tarefas: <b> 3/6 </b>
+                    </div>
+                    <div class="graph_placeholder">
+
+                    </div>
+                    <p class="graph_header-remaining_tasks"> Restam 3 Tarefas </p>
+                </section>
+                <section class="list">
+                    <div class="list_header">
+                        <select class="list_header-select">
+                            <option value="1">
+                                Todas as Tarefas
+                            </option>
+                        </select>
+                    </div>
+                </section>
+            </main>
+        </div>
+    </div>
 </body>
 
 </html>
