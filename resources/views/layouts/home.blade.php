e011b7adfb7e81d9f31356ede783a994fdeef2aa
Elias Lorandi <lorandi.elias@gmail.com>
2024-06-05 16:45:07 -0300

    Continuação layout e donwload de ícones

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -14,22 +14,32 @@
 </head>
 
 <body>
+
+
     <div class="container">
         <div class="sidebar">
             Logo
         </div>
+
         <div class="content">
             <nav>
                 <a href="#" class="btn btn-primary">
                     Nova Tarefa
                 </a>
             </nav>
+
             <main>
+
                 <section class="graph">
+
                     <div class="graph_header">
                         <h2> Progresso </h2>
-                        <hr class="line_header" />
-                        data
+                        <div class="graph_header-line"></div>
+                        <div class="graph_header-date">
+                            <img src="/assets/images/prev-icon.png" alt="">
+                            06 de Jun
+                            <img src="/assets/images/next-icon.png" alt="">
+                        </div>
                     </div>
                     <div class="graph_header-subtitle">
                         Tarefas: <b> 3/6 </b>
@@ -37,36 +47,58 @@
                     <div class="graph_placeholder">
 
                     </div>
-                    <p class="graph_header-remaining_tasks"> Restam 3 Tarefas </p>
+                    <div class="graph_header-tasks_left_footer">
+                        <img src="/assets/images/info_icon.png">
+                        Restam 3 tarefas
+                    </div>
+
                 </section>
+
+
                 <section class="list">
+
                     <div class="list_header">
                         <select class="list_header-select">
                             <option value="1">
-                                Todas as Tarefas
+                                Todas as tarefas
                             </option>
                         </select>
                     </div>
+
                     <div class="task_list">
+
                         <div class="task">
                             <div class="title">
                                 <input type="checkbox">
                                 <div class="task_title"> Titulo da Tarefa </div>
                             </div>
                             <div class="priority">
-                                <div class="sphere">
-                                    <div> Titulo da Tarefa </div>
-                                </div>
+                                <div class="sphere"></div>
+                                <div> Titulo da Tarefa </div>
                             </div>
+
                             <div class="actions">
-                                editar - excluir
+                                <a href="#">
+                                    <img src="/assets/images/edit_icon.png">
+                                </a>
+                                <a href="#">
+                                    <img src="/assets/images/trash_icon.png">
+                                </a>
                             </div>
                         </div>
+
                     </div>
+
                 </section>
+
+
             </main>
+
         </div>
+
     </div>
+
+
 </body>
 
 </html>
