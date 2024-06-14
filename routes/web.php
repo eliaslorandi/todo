bf41836abf882c81e2bc5e65facdd8881c59bf6d
Elias Lorandi <lorandi.elias@gmail.com>
2024-06-14 16:19:30 -0300

    Proteção de rotas com middleware

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,23 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('/task', [TaskController::class, 'index'])->name('create.view');
-Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
-Route::post('/task/create_action', [TaskController::class, 'create_action'])->name('task.create_action');
-Route::get('/task/edit', [TaskController::class, 'edit'])->name('task.edit');
-route::post('/task/edit_action', [TaskController::class, 'edit_action'])->name('task.edit_action');
-Route::get('/task/delete', [TaskController::class, 'delete'])->name('task.delete');
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+
+    Route::get('/task', [TaskController::class, 'index'])->name('create.view');
+    Route::get('/task/new', [TaskController::class, 'create'])->name('task.create');
+    Route::post('/task/create_action', [TaskController::class, 'create_action'])->name('task.create_action');
+    Route::get('/task/edit', [TaskController::class, 'edit'])->name('task.edit');
+    route::post('/task/edit_action', [TaskController::class, 'edit_action'])->name('task.edit_action');
+    Route::get('/task/delete', [TaskController::class, 'delete'])->name('task.delete');
+
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    
+});
+
 
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login_action'])->name('user.login_action');
 Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'register_action'])->name('user.register_action');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
