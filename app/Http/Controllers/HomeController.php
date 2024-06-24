ce91f443150e5c6c7d7a6b50729c1efe438fd65c
Elias Lorandi <lorandi.elias@gmail.com>
2024-06-24 18:20:54 -0300

    Filtro de data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,9 +9,20 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index(Request $request){
-        $tasks = Task::all()->take(3);
-        $AuthUser = Auth::user();
 
-        return view('home', ['tasks' => $tasks, 'AuthUser' => $AuthUser]);
+        //dd($request->date);
+        if($request->date){
+            $filteredDate = $request->date;
+        } else {
+            $filteredDate = date('Y-m-d');
+        }
+
+        $data['tasks'] = Task::whereDate('due_date', $filteredDate)->get();
+        $data['AuthUser'] = Auth::user();
+
+        $data['tasks_count'] = $data['tasks']->count();
+        $data['undone_tasks_count'] = $data['tasks']->where('is_done', false)->count();
+
+        return view('home', $data);
     }
 }
