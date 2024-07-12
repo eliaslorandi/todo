39c0758f7d53be23a4d27e9aeba9248395ae8684
Elias Lorandi <lorandi.elias@gmail.com>
2024-07-12 16:29:22 -0300

    Componentização da navbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Carbon::setLocale($this->app->getLocale());//app_locale já definido como pt_BR em app.php
-    }
+
+         // Registre seus componentes personalizados
+         Blade::component('components.form.text_input', 'form.text_input');
+         Blade::component('components.form.checkbox_input', 'form.checkbox_input');
+         Blade::component('components.form.select_input', 'form.select_input');
+         Blade::component('components.form.text_area_input', 'form.text_area_input');
+         Blade::component('components.button.button', 'form.button');
+         Blade::component('components.button.form_button', 'form.form_button');
+         Blade::component('components.navbar.navbar', 'navbar');
+     }
 }
---
+++ resources/views/components/navbar/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+    <div class="logo">
+        <img src="/assets/images/project-logo.png" alt="Logo">
+    </div>
+    <div class="nav-links">
+        {{ $slot }}
+    </div>
+</nav>
