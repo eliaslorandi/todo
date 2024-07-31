4c473843411d348c3ac9d18494c2cdcf082dd363
Elias Lorandi <lorandi.elias@gmail.com>
2024-07-31 11:12:50 -0300

    Adicionado o ícone do projeto

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $page ?? 'ToDo Project' }}</title>
+    <link rel="shortcut icon" href="/assets/images/project-icon.ico">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
