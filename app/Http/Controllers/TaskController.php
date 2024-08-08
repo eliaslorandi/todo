5f53cad13c3248c526c75c35cc0489e13c7d7c5e
Elias Lorandi <lorandi.elias@gmail.com>
2024-08-08 15:34:43 -0300

    Fix:
    - reajustada a logica de associação de categoria com usuário.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    public function index(){
-
+    public function index()
+    {
     }
 
-    public function create(Request $request){
+    public function create(Request $request)
+    {
         $userId = Auth::id();
         $categories = Category::where('user_id', $userId)->get();
         $data['categories'] = $categories;
@@ -21,14 +23,21 @@ class TaskController extends Controller
         return view('tasks.create_task', $data);
     }
 
-    public function create_action(Request $request) {
+    public function create_action(Request $request)
+    {
         $request->validate([
             'title' => 'required|string|max:100',
             'due_date' => 'required|date',
-            'category_id' => 'required|exists:categories,id' . Auth::id(), //exige que valor exista na tabela categories, coluna id
+            'category_id' => [
+                'required',
+                //este codigo verifica se o category_id fornecido no formulario:
+                Rule::exists('categories', 'id')->where(function ($query) { //assegura que o campo 'categort_id' durante a criação ou atualização de uma tarefa corresponde a uma categoria que realmente existe no banco de dados e que pertence ao usuário autenticado. 
+                    return $query->where('user_id', Auth::id());
+                })
+            ],
             'description' => 'required|string',
         ]);
-      
+
         //Coleta as infos da tarefa e vincula ao usuario
         $task = $request->only(['title', 'due_date', 'category_id', 'description']);
         $task['user_id'] = Auth::id();
@@ -39,9 +48,10 @@ class TaskController extends Controller
         return redirect(route('home'));
     }
 
-    public function edit(Request $request){
+    public function edit(Request $request)
+    {
         $id = $request->id;
-        
+
         $task = Task::find($id);
         if (!$task) {
             return redirect(route('home'));
@@ -53,7 +63,8 @@ class TaskController extends Controller
         return view('tasks.edit', $data);
     }
 
-    public function edit_action(Request $request){
+    public function edit_action(Request $request)
+    {
         $request_data = $request->only(['title', 'due_date', 'category_id', 'description']);
         $request_data['is_done'] = $request->is_done ? true : false;
         $task = Task::find($request->id);
@@ -66,25 +77,25 @@ class TaskController extends Controller
     }
 
     public function update(Request $request)
-{
-    $task = Task::findOrFail($request->taskId);
-    if (!$task) {
-        return response()->json(['success' => false, 'message' => 'Task não encontrada'], 404);
+    {
+        $task = Task::findOrFail($request->taskId);
+        if (!$task) {
+            return response()->json(['success' => false, 'message' => 'Task não encontrada'], 404);
+        }
+        $task->is_done = $request->status ? 1 : 0;
+        $task->save();
+
+        return response()->json(['success' => true, 'task' => $task]);
     }
-    $task->is_done = $request->status ? 1 : 0;
-    $task->save();
-
-    return response()->json(['success' => true, 'task' => $task]);
-}
 
 
-    public function delete(Request $request){
+    public function delete(Request $request)
+    {
         $id = $request->id;
         $task = Task::find($id);
-        if($task){
+        if ($task) {
             $task->delete();
         }
         return redirect(route('home'));
     }
-
 }
